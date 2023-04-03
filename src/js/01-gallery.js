cd100239ac838f637db505371ba98d304e32d023
ivan-2908 <109952917+ivan-2908@users.noreply.github.com>
2023-04-03 15:27:53 +0300

    homework-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from "SimpleLightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEll = document.querySelector(".gallery");
+
+function createGalleryCardsMarkup(items) {
+  return items.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `).join("");
+}
+
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryEll.insertAdjacentHTML("beforeend", galleryCardsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);

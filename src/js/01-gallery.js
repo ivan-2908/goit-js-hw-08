b204446095e20790c97aa512d84d727e37c05705
ivan-2908 <109952917+ivan-2908@users.noreply.github.com>
2023-04-03 17:28:57 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
 
 import { galleryItems } from './gallery-items';
 
-import SimpleLightbox from "SimpleLightbox";
+import SimpleLightbox from "simplelightbox";
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryEll = document.querySelector(".gallery");
